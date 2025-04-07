42056119ae24f54ce6d3280bceed310f2f34b679
Vinod663 <vinodfernando048@gmail.com>
2025-04-07 15:17:16 +0530

    Implement Runnable Interface

---
+++ 01.Introduction/src/main/java/org/example/ImplementRunnableInterface.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class ImplementRunnableInterface {
+    public static void main(String[] args) {
+        MyNewThread myNewThread = new MyNewThread();
+        Thread thread = new Thread(myNewThread);
+        thread.start();
+    }
+}
+
+class MyNewThread implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("My new Thread");
+    }
+}
